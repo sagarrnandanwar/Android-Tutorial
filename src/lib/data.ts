4b563a349d0a0dffeb6b5349fee4d9905a7b8fd5
ptsdpatient <119649887+ptsdpatient@users.noreply.github.com>
2024-07-23 12:32:50 +0530

    aaa js is not working

---
+++ src/lib/data.ts
@@ -0,0 +1,45 @@
+interface data {
+    markdown:String;
+    code:String;
+    output:String;
+    outputIMG:String;
+}
+
+export let Data : data [] =[
+    {
+        markdown:"Hello this is Tanishq Dhote",
+        code:"export let tanishq = 'Tanishq Dhote'",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },{
+        markdown:"",
+        code:"",
+        output:"",
+        outputIMG:""
+    },
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script>
   
      import './prism-dark.css'
-
+     import {Data} from "../lib/data"
    
 </script>
  
-<div class="w-11/12 md:w-1/2 bg-gray-900 hiddenDiv rounded-xl  mx-auto flex gap-2 flex-col shadow-xl justify-around " style="min-height:3rem;;overflow-x:auto" >
-    <div class="flex flex-row justify-between bg-gradient-to-b from-black  py-1 px-1">
-        <div class="py-1 px-3 opacity-80">
-            
+
+{#each Data as data}
+    <div class="w-11/12 md:w-1/2 bg-gray-900 hiddenDiv rounded-xl my-3  mx-auto flex gap-2 flex-col shadow-xl justify-around " style="min-height:3rem;;overflow-x:auto" >
+        <div class="flex flex-row justify-between bg-gradient-to-b from-black  py-1 px-1">
+            <div class="py-1 px-3 opacity-80">
+                
+            </div>
+            <button class="text-gray-200 tet-xl transform hover:bg-gray-700 hover:text-white py-1 px-3 rounded-lg">copy 📄</button>
         </div>
-        <button class="text-gray-200 tet-xl transform hover:bg-gray-700 hover:text-white py-1 px-3 rounded-lg">copy 📄</button>
+        <div class="p-3">
+            <pre class="whitespace-nowrap" style=""><code class="language-js text-base" >{data.code}</code></pre>
+        </div>
     </div>
-    <div class="p-3">
-        <pre class="whitespace-nowrap" style=""><code class="language-js text-base" >Hello World</code></pre>
-    </div>
-</div>
+{/each}
+    
