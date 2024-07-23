593315a368240b5d29d7ade23a3c3f6005c83d92
ptsdpatient <119649887+ptsdpatient@users.noreply.github.com>
2024-07-23 12:25:50 +0530

    code and prism init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  
+     import './prism-dark.css'
+
+   
+</script>
+ 
+<div class="w-11/12 md:w-1/2 bg-gray-900 hiddenDiv rounded-xl  mx-auto flex gap-2 flex-col shadow-xl justify-around " style="min-height:3rem;;overflow-x:auto" >
+    <div class="flex flex-row justify-between bg-gradient-to-b from-black  py-1 px-1">
+        <div class="py-1 px-3 opacity-80">
+            
+        </div>
+        <button class="text-gray-200 tet-xl transform hover:bg-gray-700 hover:text-white py-1 px-3 rounded-lg">copy 📄</button>
+    </div>
+    <div class="p-3">
+        <pre class="whitespace-nowrap" style=""><code class="language-js text-base" >Hello World</code></pre>
+    </div>
+</div>
